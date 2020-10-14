d9fc639182545bec0cb44d93b27dc4b811e26c96
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2020-10-13 18:55:16 -0700

    add angular-material, less & sass compilation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import angular from "angular";
+import "angular";
+import "angular-animate";
+import "angular-material";
+import "@uirouter/angularjs";
 
+import "./app";
 import "./module";
+import "./styles/index.scss";
+import "./styles/index.less";
+import "angular-material/angular-material.css";
